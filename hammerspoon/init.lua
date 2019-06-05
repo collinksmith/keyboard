d23405d3a149619f6a7f990294e478d9970a872b
Collin Smith <c.smith@guideline.com>
2019-06-05 13:56:27 -0500

    Remove hotkey to reload hs config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,11 +2,6 @@ require("hs.ipc")
 hs.ipc.cliInstall()
 local log = hs.logger.new('init.lua', 'debug')
 
--- Use Control+` to reload Hammerspoon config
-hs.hotkey.bind({'ctrl'}, '`', nil, function()
-  hs.reload()
-end)
-
 keyUpDown = function(modifiers, key)
   -- Un-comment & reload config to log each keystroke that we're triggering
   -- log.d('Sending keystroke:', hs.inspect(modifiers), key)
